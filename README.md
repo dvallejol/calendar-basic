e10531f294f2dea7973c58c756a2d4afdfba3e48
dijolev <diegovallejocei@gmail.com>
2024-07-16 05:33:09 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#Backend MERN - Calendar "MERN": Uknown word.
+
+## Dev
+
+1. Instalar dependencias `npm install`
+2. Clonar el archivo `.env.template` y renombrarlo a `.env`
+3. cambiar las variables de entorno acorde a la configuración
+4. Correr el servidor `npm start`
+
---
+++ models/Evento.js
@@ -0,0 +1,40 @@
+const { type } = require('express/lib/response');
+const { Schema, model} =require ('mongoose');
+const { start } = require('repl');
+
+
+const EventoSchema = Schema({
+    
+    title: {
+        type: String,
+        required: true
+    },
+    notes: {
+        type: String,
+    },
+    start:{
+        type: Date,
+        required: true
+    },
+    end: {
+        type: Date,
+        required: true
+    },
+    user: {
+       type: Schema.Types.ObjectId,
+       ref: 'Usuario',
+       required: true
+    }
+
+    
+
+});
+
+EventoSchema.method('toJSON', function(){
+    const {__v, _id, ...Object} = this.toObject();
+    Object.id = _id;
+    return Object;
+
+})
+
+module.exports = model('Evento', EventoSchema);
